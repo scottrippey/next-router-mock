f01d7bc3496d7706d5f866d79c91a40b72410fe1
Julien Prugne <julien@webeleon.dev>
2021-07-26 07:42:05 +0200

    test: convert inlineSnapshot to matchObject

--- src/MemoryRouter.test.tsx
+++ src/MemoryRouter.test.tsx
@@ -2,45 +2,33 @@ import { MemoryRouter } from "./MemoryRouter";
 
 describe("MemoryRouter", () => {
   const memoryRouter = new MemoryRouter();
-  function currentRoute() {
-    const { pathname, query, asPath, locale } = memoryRouter;
-
-    return { pathname, query, asPath, locale };
-  }
 
   it("should start empty", () => {
-    expect(currentRoute()).toMatchInlineSnapshot(`
-      Object {
-        "asPath": "",
-        "locale": undefined,
-        "pathname": "",
-        "query": Object {},
-      }
-    `);
+    expect(memoryRouter).toMatchObject({
+      asPath: "",
+      pathname: "",
+      query: {},
+      locale: undefined,
+    })
   });
   it("pushing URLs should update the route", () => {
     memoryRouter.push(`/one/two/three`);
-    expect(currentRoute()).toMatchInlineSnapshot(`
-      Object {
-        "asPath": "/one/two/three",
-        "locale": undefined,
-        "pathname": "/one/two/three",
-        "query": Object {},
-      }
-    `);
+
+    expect(memoryRouter).toMatchObject({
+      asPath: "/one/two/three",
+      pathname: "/one/two/three",
+      query: {},
+    })
 
     memoryRouter.push(`/one/two/three?four=4&five=`);
-    expect(currentRoute()).toMatchInlineSnapshot(`
-      Object {
-        "asPath": "/one/two/three?four=4&five=",
-        "locale": undefined,
-        "pathname": "/one/two/three",
-        "query": Object {
-          "five": "",
-          "four": "4",
-        },
-      }
-    `);
+    expect(memoryRouter).toMatchObject({
+      asPath: "/one/two/three?four=4&five=",
+      pathname: "/one/two/three",
+      query: {
+        five: "",
+        four: "4",
+      },
+    });
   });
   it("pushing should trigger the routeChangeComplete event", () => {
     const routeChangeComplete = jest.fn();
@@ -58,96 +46,73 @@ describe("MemoryRouter", () => {
 
   it("pushing UrlObjects should update the route", () => {
     memoryRouter.push({ pathname: "/one" });
-    expect(currentRoute()).toMatchInlineSnapshot(`
-      Object {
-        "asPath": "/one",
-        "locale": undefined,
-        "pathname": "/one",
-        "query": Object {},
-      }
-    `);
+    expect(memoryRouter).toMatchObject({
+        asPath: "/one",
+        pathname: "/one",
+        query: {},
+    });
 
     memoryRouter.push({
       pathname: "/one/two/three",
       query: { four: "4", five: "" },
     });
-    expect(currentRoute()).toMatchInlineSnapshot(`
-      Object {
-        "asPath": "/one/two/three?four=4&five=",
-        "locale": undefined,
-        "pathname": "/one/two/three",
-        "query": Object {
-          "five": "",
-          "four": "4",
+    expect(memoryRouter).toMatchObject({
+        asPath: "/one/two/three?four=4&five=",
+        pathname: "/one/two/three",
+        query: {
+          five: "",
+          four: "4",
         },
-      }
-    `);
+      });
   });
   it("pushing UrlObjects should inject slugs", () => {
     memoryRouter.push({ pathname: "/one/[id]", query: { id: "two" } });
-    expect(currentRoute()).toMatchInlineSnapshot(`
-      Object {
-        "asPath": "/one/two",
-        "locale": undefined,
-        "pathname": "/one/[id]",
-        "query": Object {
-          "id": "two",
-        },
-      }
-    `);
+    expect(memoryRouter).toMatchObject({
+        asPath: "/one/two",
+        pathname: "/one/[id]",
+        query: {
+          id: "two",
+        }
+    });
 
     memoryRouter.push({ pathname: "/one/[id]/three", query: { id: "two" } });
-    expect(currentRoute()).toMatchInlineSnapshot(`
-      Object {
-        "asPath": "/one/two/three",
-        "locale": undefined,
-        "pathname": "/one/[id]/three",
-        "query": Object {
-          "id": "two",
+    expect(memoryRouter).toMatchObject({
+        asPath: "/one/two/three",
+        pathname: "/one/[id]/three",
+        query: {
+          id: "two",
         },
-      }
-    `);
+    });
 
     memoryRouter.push({
       pathname: "/one/[id]/three",
       query: { id: "two", four: "4" },
     });
-    expect(currentRoute()).toMatchInlineSnapshot(`
-      Object {
-        "asPath": "/one/two/three?four=4",
-        "locale": undefined,
-        "pathname": "/one/[id]/three",
-        "query": Object {
-          "four": "4",
-          "id": "two",
+    expect(memoryRouter).toMatchObject({
+        asPath: "/one/two/three?four=4",
+        pathname: "/one/[id]/three",
+        query:  {
+          four: "4",
+          id: "two",
         },
-      }
-    `);
+    });
     memoryRouter.push({
       pathname: "/one/[id]/three/[four]",
       query: { id: "two", four: "4" },
     });
-    expect(currentRoute()).toMatchInlineSnapshot(`
-      Object {
-        "asPath": "/one/two/three/4",
-        "locale": undefined,
-        "pathname": "/one/[id]/three/[four]",
-        "query": Object {
-          "four": "4",
-          "id": "two",
+    expect(memoryRouter).toMatchObject({
+        asPath: "/one/two/three/4",
+        pathname: "/one/[id]/three/[four]",
+        query: {
+          four: "4",
+          id: "two",
         },
-      }
-    `);
+    });
   });
   it("push the locale", () => {
-    memoryRouter.push("/", "/", { locale: "en" });
-    expect(currentRoute()).toMatchInlineSnapshot(`
-      Object {
-        "asPath": "/",
-        "locale": "en",
-        "pathname": "/",
-        "query": Object {},
-      }
-    `);
+    memoryRouter.push("/", undefined, { locale: "en" });
+    expect(memoryRouter).toMatchObject({
+        locale: "en",
+    });
   })
 });
