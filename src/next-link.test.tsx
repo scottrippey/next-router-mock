6ae6b09f337fb9e74ebaaac52e838ca185676465
scottrippey <scott.william.rippey@gmail.com>
2021-08-31 21:20:08 -0600

    feature(next/link): added more tests

--- src/next-link.test.tsx
+++ src/next-link.test.tsx
@@ -7,9 +7,25 @@ import MockRouter from './router';
 jest.mock('next/dist/client/router', () => require('./router'));
 
 describe('next/link', () => {
-  it('should render a link', () => {
-    render(<NextLink href="/example">Example Link</NextLink>);
-    fireEvent.click(screen.getByText('Example Link'));
-    expect(MockRouter.asPath).toEqual('/example');
+  describe('clicking a link will mock navigate', () => {
+    it('to a href', () => {
+      render(<NextLink href="/example?foo=bar">Example Link</NextLink>);
+      fireEvent.click(screen.getByText('Example Link'));
+      expect(MockRouter).toMatchObject({
+        asPath: '/example?foo=bar',
+        pathname: '/example',
+        query: { foo: 'bar' },
+      });
+    });
+
+    it('to a URL object', () => {
+      render(<NextLink href={{ pathname: '/example', query: { foo: 'bar' } }}>Example Link</NextLink>);
+      fireEvent.click(screen.getByText('Example Link'));
+      expect(MockRouter).toMatchObject({
+        asPath: '/example?foo=bar',
+        pathname: '/example',
+        query: { foo: 'bar' },
+      });
+    });
   });
 });
