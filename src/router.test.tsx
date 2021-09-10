4ea8b84a5b4cb1399003244fb615b70e4cc856d3
Kamran Ayub <kamran.ayub2@target.com>
2021-09-10 11:00:09 -0500

    force re-render on routeChangeStart to allow capturing previous router data

--- src/router.test.tsx
+++ src/router.test.tsx
@@ -1,6 +1,7 @@
 import { act, renderHook } from "@testing-library/react-hooks";
 import router, { useRouter } from "./router";
 import { MemoryRouter } from "./MemoryRouter";
+import { useEffect, useRef } from "react";
 
 describe("router", () => {
   it("should export a default router", () => {
@@ -14,7 +15,51 @@ describe("router", () => {
       expect(result.current).toBe(router);
     });
 
-    it('"push" will cause a rerender with the new route', async () => {
+    it("will allow capturing previous route values in hooks with routing events", async () => {
+      const mockOnRouteChange = jest.fn();
+
+      // see: https://github.com/streamich/react-use/blob/master/src/usePrevious.ts
+      const usePrevious = function <T>(value: T): T | undefined {
+        const previous = useRef<T>();
+
+        useEffect(() => {
+          previous.current = value;
+        });
+
+        return previous.current;
+      };
+
+      const useRouterWithPrevious = () => {
+        const { asPath, events } = useRouter();
+        const previousAsPath = usePrevious(asPath);
+
+        useEffect(() => {
+          const handleRouteChangeComplete = (nextUrl: string) =>
+            mockOnRouteChange(previousAsPath, nextUrl);
+
+          events.on("routeChangeComplete", handleRouteChangeComplete);
+
+          return () => {
+            events.off("routeChangeComplete", handleRouteChangeComplete);
+          };
+        }, [previousAsPath, events]);
+
+        return router;
+      };
+
+      // Push initial state
+      await router.push('/foo');
+
+      const { result } = renderHook(() => useRouterWithPrevious());
+
+      await act(async () => {
+        await result.current.push("/foo?bar=baz");
+      });
+
+      expect(mockOnRouteChange).toHaveBeenCalledWith("/foo", "/foo?bar=baz");
+    });
+
+    it('"push" will cause a rerender on routeChangeComplete with the new route', async () => {
       const { result } = renderHook(() => useRouter());
 
       expect(result.current).toBe(router);
--- src/router.tsx
+++ src/router.tsx
@@ -9,15 +9,23 @@ export default memoryRouter;
 // Overrides the useRouter hook:
 export const useRouter = () => {
   const [router, setRouter] = useState(memoryRouter);
+  const [, rerender] = useState<object>();
 
   useEffect(() => {
     const handleRouteChange = () => {
       // Clone the (mutable) memoryRouter, to ensure we trigger an update
       setRouter({ ...memoryRouter });
     };
+
+    const handleRouteStart = () => {
+      rerender({});
+    };
+
+    memoryRouter.events.on("routeChangeStart", handleRouteStart);
     memoryRouter.events.on("routeChangeComplete", handleRouteChange);
 
     return () => {
+      memoryRouter.events.off("routeChangeStart", handleRouteStart);
       memoryRouter.events.off("routeChangeComplete", handleRouteChange);
     };
   }, []);
