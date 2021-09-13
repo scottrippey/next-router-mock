3f5303a1244b14adf6d675e48d5e61eda4283dcf
scottrippey <scott.william.rippey@gmail.com>
2021-09-13 10:48:15 -0600

    feature(storybook): allow event handlers to be passed to useMemoryRouter

--- src/useMemoryRouter.tsx
+++ src/useMemoryRouter.tsx
@@ -2,9 +2,17 @@ import { useEffect, useState } from "react";
 
 import { MemoryRouter } from "./MemoryRouter";
 
-export const useMemoryRouter = (initialRouter: MemoryRouter) => {
+export type MemoryRouterEventHandlers = {
+  onRouteChange?: (url: string, options: { shallow: boolean }) => void;
+  onRouteChangeStart?: (url: string, options: { shallow: boolean }) => void;
+  onPush?: (url: string, options: { shallow: boolean }) => void;
+  onReplace?: (url: string, options: { shallow: boolean }) => void;
+};
+
+export const useMemoryRouter = (initialRouter: () => MemoryRouter, eventHandlers?: MemoryRouterEventHandlers) => {
   const [router, setRouter] = useState(initialRouter);
 
+  // Trigger updates on route changes:
   useEffect(() => {
     const handleRouteChange = () => {
       // Clone the (mutable) memoryRouter, to ensure we trigger an update
@@ -12,13 +20,33 @@ export const useMemoryRouter = (initialRouter: MemoryRouter) => {
     };
 
     router.events.on("routeChangeComplete", handleRouteChange);
-
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
+    return () => router.events.off("routeChangeComplete", handleRouteChange);
   }, [
     // Events is a singleton, shared by all clones, so this should never actually change:
     router.events,
+  ]);
+
+  useEffect(() => {
+    if (!eventHandlers) return;
+    const { onRouteChange, onRouteChangeStart, onPush, onReplace } = eventHandlers;
+    if (!(onRouteChange || onRouteChangeStart || onPush || onReplace)) return;
+
+    if (onRouteChangeStart) router.events.on("routeChangeStart", onRouteChangeStart);
+    if (onRouteChange) router.events.on("routeChangeComplete", onRouteChange);
+    if (onPush) router.events.on("NEXT_ROUTER_MOCK:push", onPush);
+    if (onReplace) router.events.on("NEXT_ROUTER_MOCK:replace", onReplace);
+    return () => {
+      if (onRouteChangeStart) router.events.off("routeChangeStart", onRouteChangeStart);
+      if (onRouteChange) router.events.off("routeChangeComplete", onRouteChange);
+      if (onPush) router.events.off("NEXT_ROUTER_MOCK:push", onPush);
+      if (onReplace) router.events.off("NEXT_ROUTER_MOCK:replace", onReplace);
+    };
+  }, [
+    router.events,
+    eventHandlers?.onRouteChange,
+    eventHandlers?.onRouteChangeStart,
+    eventHandlers?.onPush,
+    eventHandlers?.onReplace,
   ]);
 
   return router;
