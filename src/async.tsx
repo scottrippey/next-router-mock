714a7e8842193aad8615e5bfcc47057f73233656
scottrippey <scott.william.rippey@gmail.com>
2022-09-29 10:27:34 -0600

    feature(router-context): use inline exports again

--- src/async.tsx
+++ src/async.tsx
@@ -1,30 +1,28 @@
 import React from "react";
+import { MemoryRouter } from "./MemoryRouter";
 import { useMemoryRouter } from "./useMemoryRouter";
 import { withMemoryRouter, WithRouterProps } from "./withMemoryRouter";
 import { NextComponentType, NextPageContext } from "next";
 
-import { MemoryRouter, BaseRouter, Url } from "./MemoryRouter";
+// Export extra mock APIs:
+export { useMemoryRouter } from "./useMemoryRouter";
+export { MemoryRouter, BaseRouter, Url } from "./MemoryRouter";
 
-const memoryRouter = new MemoryRouter();
+export const memoryRouter = new MemoryRouter();
 memoryRouter.async = true;
+export default memoryRouter;
 
-const useRouter = () => {
+export const useRouter = () => {
   return useMemoryRouter(memoryRouter);
 };
 
-const withRouter = <P extends WithRouterProps, C = NextPageContext>(
+export const withRouter = <P extends WithRouterProps, C = NextPageContext>(
   ComposedComponent: NextComponentType<C, any, P>
 ) => {
   return withMemoryRouter(useRouter, ComposedComponent);
 };
 
-// Export the same interfaces as `next/router`:
-export default memoryRouter;
-export { useRouter, withRouter };
 // Export the same interface as `next/dist/shared/lib/router-context`
 // This is a workaround for `next/link` v12.2, since it no longer uses the `useRouter` hook.
-// Instead, we will export a context that has the default value set to our mock:
+// Instead, we can use a context that has the default value set to our mock:
 export const RouterContext = React.createContext(memoryRouter);
-
-// Export extra mock stuff:
-export { memoryRouter, useMemoryRouter, MemoryRouter, BaseRouter, Url };
