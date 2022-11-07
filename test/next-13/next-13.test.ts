f5a22adffa040a2542669282ac61e2c1083a8c4a
scottrippey <scott.william.rippey@gmail.com>
2022-11-07 10:23:47 -0700

    feature(router-context): added Next 13 tests

---
+++ test/next-13/next-13.test.ts
@@ -0,0 +1,23 @@
+// TODO: Officially support Next 13
+// Validate our types are exported correctly:
+import type { createDynamicRouteParser } from "../../dynamic-routes";
+import type { createDynamicRouteParser as _ } from "../../dynamic-routes/next-12";
+import type { MemoryRouterProvider } from "../../MemoryRouterProvider";
+import type { MemoryRouterProvider as __ } from "../../MemoryRouterProvider/next-12";
+
+describe(`next version ${require("next/package.json").version}`, () => {
+  describe("automatic and explicit import paths are valid", () => {
+    it("next-router-mock/dynamic-routes", () => {
+      require("../../dynamic-routes");
+    });
+    it("next-router-mock/dynamic-routes/next-12", () => {
+      require("../../dynamic-routes/next-12");
+    });
+    it("next-router-mock/MemoryRouterProvider", () => {
+      require("../../MemoryRouterProvider");
+    });
+    it("next-router-mock/MemoryRouterProvider/next-12", () => {
+      require("../../MemoryRouterProvider/next-12");
+    });
+  });
+});
