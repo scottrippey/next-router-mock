9a2b4d55bdb2562100d706b7ecea5b1b85e69f2f
scottrippey <scott.william.rippey@gmail.com>
2023-06-22 07:26:58 -0600

    fix(urls): support multiple-values for query parameters

--- src/urls.ts
+++ src/urls.ts
@@ -4,7 +4,12 @@ import type { UrlObject } from "./MemoryRouter";
 export function parseUrl(url: string): UrlObject {
   const base = "https://base.com"; // base can be anything
   const parsed = new URL(url, base);
-  const query = Object.fromEntries(parsed.searchParams);
+  const query = Object.fromEntries(
+    Array.from(parsed.searchParams.keys()).map((key) => {
+      const values = parsed.searchParams.getAll(key);
+      return [key, values.length === 1 ? values[0] : values];
+    })
+  );
   return {
     pathname: parsed.pathname,
     hash: parsed.hash,
@@ -12,5 +17,12 @@ export function parseUrl(url: string): UrlObject {
   };
 }
 export function stringifyQueryString(query: NextRouter["query"]): string {
-  return new URLSearchParams(query as Record<string, string>).toString();
+  const params = new URLSearchParams();
+  Object.keys(query).forEach((key) => {
+    const values = query[key];
+    for (const value of Array.isArray(values) ? values : [values]) {
+      params.append(key, value!);
+    }
+  });
+  return params.toString();
 }
