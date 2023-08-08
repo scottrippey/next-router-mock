3ff403143c96b1a645898671d6c11f3390449a23
scottrippey <scott.william.rippey@gmail.com>
2023-08-08 10:05:14 -0600

    feat(example-app): updated example tests

--- test/example-app/components/Search.stories.tsx
+++ test/example-app/components/Search.stories.tsx
@@ -1,15 +1,14 @@
-import { MemoryRouterProvider } from "../../../src/MemoryRouterProvider/next-13";
+import { MemoryRouterProvider } from "next-router-mock/MemoryRouterProvider/next-13";
 import { Search } from "./Search";
+
+const MemoryRouterDecorator = (Story: React.ComponentType) => (
+  <MemoryRouterProvider>
+    <Story />
+  </MemoryRouterProvider>
+);
 
 export default {
   title: "components / Search",
+  component: Search,
+  decorators: [MemoryRouterDecorator],
 };
-
-export const WithDecorator = () => <Search />;
-WithDecorator.decorators = [
-  (Story) => (
-    <MemoryRouterProvider>
-      <Story />
-    </MemoryRouterProvider>
-  ),
-];
