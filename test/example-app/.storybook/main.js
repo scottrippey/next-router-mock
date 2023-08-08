7f020e88e781852e29fec9c52083bdb3e1abbe85
scottrippey <scott.william.rippey@gmail.com>
2023-08-08 10:04:57 -0600

    feat(example-app): clean up unused Storybook

--- test/example-app/.storybook/main.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  stories: ["../components/**/*.stories.@(mdx|js|jsx|ts|tsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
-  core: {
-    builder: "webpack5",
-  },
-};
