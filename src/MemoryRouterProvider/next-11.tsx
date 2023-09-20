8a06e7db4071c9c86673bf5b7bb62dcce5ce9915
Scott Rippey <scottrippey@users.noreply.github.com>
2023-09-20 10:04:21 -0600

    feature(next-13.5): improve compatibility comment

--- src/MemoryRouterProvider/next-11.tsx
+++ src/MemoryRouterProvider/next-11.tsx
@@ -1,4 +1,4 @@
-// NOTE: this path works with Next v11.1.0+
+// NOTE: this path works with Next v11.1.0 through v13.4.0+
 // @ts-ignore
 import { RouterContext } from "next/dist/shared/lib/router-context";
 import { factory } from "./MemoryRouterProvider";
